af39a5ca026a24a3d6942f5c58a4290ed74d2f71
kelly-riv <kelly.rivera@inacapmail.cl>
2023-08-31 22:51:43 -0400

    avance tercer commit

---
+++ app/src/main/java/com/example/floreriamartina/Display.java
@@ -0,0 +1,12 @@
+package com.example.floreriamartina;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Display extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+    }
+}
